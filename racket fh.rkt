9e6ff57d856aa0db8a5aef933a0997f4e99194d3
holandafelipeuff <felipeholanda@id.uff.br>
2018-11-07 18:33:13 -0200

    mudança para realmente salvar na lista interna
    Antes não estava salvando na lista interna, agr com o set! está

--- racket fh.rkt
+++ racket fh.rkt
@@ -1,43 +1,39 @@
 #lang racket
+
 (require graph)
 
-(define g (weighted-graph/directed '((x a b) (y b c))))
+(define g (weighted-graph/directed '((a x y) (b y z))))
 
-(define pdl "x;y")
+(define pdl "a;b")
 
 (define (verificaPDLGrafo programaPDL i grafo listaInterna)
   
     (cond 
      
-      [(char=? (string-ref programaPDL i) #\x) ;Se o caractere na posição atual é igual a "x"
-        (display "aaaaaaaaaaaaaaaaaaaaaa")
-        (append listaInterna (list(string-ref programaPDL i)) ;Coloca no final da listaInterna o valor achado
-        (verificaPDLGrafo programaPDL (+ i 1) grafo listaInterna))
+      [(char=? (string-ref programaPDL i) #\a) ;Se o caractere na posição atual é igual a "a"
+        (set! listaInterna (append listaInterna (list(string-ref programaPDL i)))) ;Coloca no final da listaInterna o valor achado
+        (verificaPDLGrafo programaPDL (+ i 1) grafo listaInterna)
       ]
 
-      [(char=? (string-ref programaPDL i) #\y) ;Se o caractere na posição atual é igual a "y"
-        (append listaInterna (list(string-ref programaPDL i)) ;Coloca no final da listaInterna o valor achado
-        (verificaPDLGrafo programaPDL (+ i 1) grafo listaInterna))
+      [(char=? (string-ref programaPDL i) #\b) ;Se o caractere na posição atual é igual a "b"
+        (set! listaInterna (append listaInterna (list(string-ref programaPDL i)))) ;Coloca no final da listaInterna o valor achado
+        (verificaPDLGrafo programaPDL (+ i 1) grafo listaInterna)
       ]
 
       [(char=? (string-ref programaPDL i) #\U) ;Se o caractere na posição atual é igual a "U"
-        (append listaInterna (list(string-ref programaPDL i)) ;Coloca no final da listaInterna o valor achado
-        (verificaPDLGrafo programaPDL (+ i 1) grafo listaInterna))
+        (set! listaInterna (append listaInterna (list(string-ref programaPDL i)))) ;Coloca no final da listaInterna o valor achado
+        (verificaPDLGrafo programaPDL (+ i 1) grafo listaInterna)
       ]
 
       [(char=? (string-ref programaPDL i) #\*) ;Se o caractere na posição atual é igual a "*"
-        (append listaInterna (list(string-ref programaPDL i)) ;Coloca no final da listaInterna o valor achado
-        (verificaPDLGrafo programaPDL (+ i 1) grafo listaInterna))
+        (set! listaInterna (append listaInterna (list(string-ref programaPDL i)))) ;Coloca no final da listaInterna o valor achado
+        (verificaPDLGrafo programaPDL (+ i 1) grafo listaInterna)
       ]
 
       [(char=? (string-ref programaPDL i) #\;) ;Se o caractere na posição atual é igual a ";"
-        (display listaInterna)      ]
-     
-      ;[(not (char=? (string-ref programaPDL i) #\;)) ;se não, percorre a palavra até achar
-      ;  (if (not (= (- (string-length programaPDL) 1) i)) ; condição de parada: i = string-length
-      ;    (verificaPDLGrafo programaPDL (+ i 1)) ; imprime caracteres recursivamente
-      ;    (display "" ))]
-
+        (display (length listaInterna))
+      ]
+   
     )
   
 )
